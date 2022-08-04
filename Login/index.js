72939f5676e8b91ac21efafd56a1ff2c5a31d34a
LuizHenrique000 <luizlhra24@gmail.com>
2022-08-04 14:12:44 -0300

    Implementa funcionalidade no botão Saiba mais

---
+++ Login/index.js
@@ -0,0 +1,6 @@
+function mostrarTexto() {
+    var container = document.querySelector('.texto-recaptcha');
+    var button = document.querySelector('.saiba')
+    container.style.display = 'block';
+    button.style.display = 'none';
+}
